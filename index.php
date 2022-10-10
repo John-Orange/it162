08fda1378c1aff07027ba1c179d702c6822c3243
Zhongyuan Huang <zhongyuan.huang1996@gmail.com>
2022-10-10 13:09:45 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include "includes/header.php";?>
+
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome</h2>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.png" class="desktop" alt="myself">
+ <img src="images/phone.png" class="phone" alt="bird">
+ <img src="images/tablet.png" class="tablet" alt="face">
+ <p>Hello! My name is Zhongyuan Huang or you can call me John. This is my second year in Seattle Central College. I am marjoing in IT and probably would focus in programing.</p>
+ <p>I love to stay home play video games; I also like to hiking to see the most beautiful views.</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<!--<aside>
+ <h3>Right Column</h3>
+ <img src="images/tablet.npg" class="tablet" alt="happyface" />
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+</aside>-->
+<!-- END RIGHT COL -->
+
+<?php include "includes/footer.php";?>
