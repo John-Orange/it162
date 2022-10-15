7f9ed101a00362865fa013d36529a6bc4e630fd8
Zhongyuan Huang <zhongyuan.huang1996@gmail.com>
2022-10-14 17:23:04 -0700

    form

--- contactme.php
+++ contactme.php
@@ -1,4 +1,36 @@
 <?php include "includes/header.php";?>
 
+   
+    <!--[if ltIE9]>
+       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+     <![endif]-->
+	 <header><h3>Contact Us!</h3></header>
+    <p>Inside the source of this file, you can un-comment and test 
+        2 different types of forms:
+        <div class="indent">
+            <ul>
+                <li><b>loadContact('simple.php');</b> - a simple contact form with reCAPTCHA</li>
+                <li><b>loadContact('multiple.php');</b> - a complex form with reCAPTCHA</li>
+            </ul>
+        </div>    
+    
+	<?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
 
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "zhongyuan.huang1996@gmail.com";  //place your/your client's email address here
+        $toName = "John"; //place your client's name here
+        $website = "orangeandjasmine";  //place NAME of your client's website
+
+        echo loadContact('simple.php');#demonstrates a simple contact form
+        //echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>
 <?php include "includes/footer.php";?>
+
